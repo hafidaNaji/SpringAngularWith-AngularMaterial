75d50c90237d57689f9243468187e0e2c058dcd2
hafida <hafidanaji13@gmail.com>
2024-06-06 13:23:35 +0100

    first Commit

---
+++ frontend-ang/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {MatButton, MatButtonModule, MatIconButton} from "@angular/material/button";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatMenu, MatMenuModule} from "@angular/material/menu";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoadStudentsComponent } from './load-students/load-students.component';
+import { LoadPaymentsComponent } from './load-payments/load-payments.component';
+import { LoginComponent } from './login/login.component';
+import { StudentsComponent } from './students/students.component';
+import { PaymentsComponent } from './payments/payments.component';
+
+import {MatCard, MatCardModule} from "@angular/material/card";
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminTemplateComponent,
+    HomeComponent,
+    ProfileComponent,
+    LoadStudentsComponent,
+    LoadPaymentsComponent,
+    LoginComponent,
+    StudentsComponent,
+    PaymentsComponent,
+    DashboardComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
